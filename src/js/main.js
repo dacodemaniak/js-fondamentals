895744e852cf45b5309a999805e408127c163193
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2025-09-02 09:41:50 +0200

    Pure css design

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+/**
+ * displayTitle
+ * @param void
+ * @returns void
+ * Just display some string in the main HTML page title
+ */
+const displayTitle = () => {
+    const titleSlot = document.querySelector('h1')
+    titleSlot.innerText = 'Todolist'
+}
+
+/**
+ * Self callable function
+ */
 (() => {
-    console.log(`I was executed`)
+    displayTitle()
 })()
+
